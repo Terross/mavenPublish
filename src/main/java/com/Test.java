413b3ccd248c93a9cf1b2cc56842be708ef5e187
Terross <dima38091@gmail.com>
2021-09-14 13:45:41 +0300

    first commit

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,7 +1,13 @@
 package com;
 
 public class Test {
+    public Test() {
+        System.out.println(12345);
+    }
     public void test() {
         System.out.println("123");
+    }
+    public static void what() {
+        System.out.println(0);
     }
 }
